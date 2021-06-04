7895f95d1f5fcc1a37df9b97393abfd06a215451
seb-tdna <85258395+seb-tdna@users.noreply.github.com>
2021-06-04 14:00:51 -0700

    Update curve.js

--- Components/curve.js
+++ Components/curve.js
@@ -64,7 +64,7 @@ export default function Curve(props) {
         ctx.stroke()
         ctx.font = "300 22px arial"
         ctx.beginPath()
-        ctx.fillText(text, point.X - (ctx.measureText(text).width / 2 - 10), point.Y + 40)
+        ctx.fillText(text, point.X - (ctx.measureText(text).width / 2 - 7), point.Y + 50)
         ctx.stroke()
     }
 
